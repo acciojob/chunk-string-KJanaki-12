121ab63fb7bfa86622dbe3073243ddfc0b8c6711
KJanaki-12 <kenchugundujanaki@gmail.com>
2025-09-29 20:14:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function stringChop(str, size) {
   // your code here
+	let ans = [];
+	for(let i=0;i<str.length;i=i+size){
+		ans.push(str.slice(i, i+size));
+	}
+	return ans;
 }
+
+
 
 // Do not change the code below
 const str = prompt("Enter String.");
