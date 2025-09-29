db760c9b0cfd89f4e8b92aefef0a55ed20a8f8a5
KJanaki-12 <kenchugundujanaki@gmail.com>
2025-09-29 20:31:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 function stringChop(str, size) {
   // your code here
 	let ans = [];
-	if(str.length === 0){
-		return [];
-	}else{
 		for(let i=0;i<str.length;i=i+size){
 			ans.push(str.slice(i, i+size));
 		}
-		return ans;
-	}
+	let result = ans.push(...);
+	return result;
+		
 }
 
 // Do not change the code below
